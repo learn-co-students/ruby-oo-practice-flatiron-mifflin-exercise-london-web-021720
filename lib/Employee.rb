6bc05007093157d799e7a679c269cc319b5509be
Amaterasus <skyetay03@googlemail.com>
2020-02-24 18:15:42 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,32 @@
 class Employee
 
+    @@all = []
+
+    attr_reader :name, :salary, :manager_name
+
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(monies)
+        all.select { |employee| employee.salary > monies}
+    end
+
+    def self.find_by_department(department)
+        department_manager = Manager.all.find { |manager| manager.department == department}
+        department_manager.employees[0]
+    end
+
+    def tax_bracket
+        Employee.all.select { |employee| employee.salary >= self.salary - 1000 && employee.salary <= self.salary + 1000}
+    end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,38 @@
 class Manager
 
+    attr_reader :name, :department, :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @employees = []
+        @@all << self
+    end
+
+    def employees
+        @employees
+    end
+
+    def hire_employee(employee_name, salary)
+        @employees << Employee.new(employee_name, salary, self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+        all.map { |manager| manager.department}
+    end
+
+    def self.manager_ages
+        all.map { |manager| manager.age }
+    end
+
+    def self.average_age
+        manager_ages.sum.to_f / all.size
+    end
 end
