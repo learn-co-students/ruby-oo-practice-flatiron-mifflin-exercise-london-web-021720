aebb5473cc0e20ed5215c75d8a1b69648dc05112
nickzaf95 <nicholaszafiriou@gmail.com>
2020-02-24 15:46:32 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,29 @@
 class Employee
+    attr_accessor :name, :manager_name
+    attr_reader :salary
+
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(number)
+        @@all.select{ |employee| employee.salary > number }
+    end
+
+    def self.find_by_department(string)
+        (@@all.find{ |employee| employee.manager_name.department == string })
+    end
+
+    def tax_bracket
+        @@all.select{ |employee| (((employee.salary)+1000)-self.salary) < 2000 }
+    end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
+    attr_accessor :name, :department
+    attr_reader :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def employees
+        Employee.all.select{ |employee| employee.manager_name == self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(employee, salary)
+        hire = Employee.new(employee, salary)
+        hire.manager_name = self
+    end
+
+    def self.all_departments
+        x = @@all.map { |manager| manager.department }
+        x.uniq
+    end
+
+    def self.average_age
+        x = @@all.map{ |manager| manager.age }
+        y = x.sum
+        z = y / (@@all.size).to_f
+        z
+    end
+
 
 end
